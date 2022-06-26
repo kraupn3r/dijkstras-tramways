835d2a32c87eba28b1b230c4117c20c065115e23
m <m.pyzowski@o2.pl>
2022-06-26 15:54:08 +0200

    last commit

--- docker/backend_setup.sh
+++ docker/backend_setup.sh
@@ -3,7 +3,6 @@
 yes | python manage.py makemigrations # This is a development thing so I don't have to run it manually, but will be removed in production
 yes | python manage.py migrate
 yes | python manage.py loaddata data.json
-
 yes | python manage.py creatematrices
 # yes | python manage.py dumpdata -e contenttypes -e auth.Permission --indent 4 > dumpo.json
 
